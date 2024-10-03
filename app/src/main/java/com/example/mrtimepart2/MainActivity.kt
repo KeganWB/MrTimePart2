9156ae606c8a5521d07dec0f9f2c528ce33685bd
AmLisBack <109963053+AmLisBack@users.noreply.github.com>
2024-10-03 15:23:32 +0200

    setMainContentView

--- app/src/main/java/com/example/mrtimepart2/MainActivity.kt
+++ app/src/main/java/com/example/mrtimepart2/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
     val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
+        setContentView(R.layout.activity_main)
     }
 }
 
